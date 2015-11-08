2f1f7f5ab356278717f351e63ffd4d44865ec8e9
Matt Cholick <cholick@gmail.com>
2015-11-08 15:35:59 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Alfred UUID Workflow
+
+Simple workflow to generate a random uuid and put it on the clipboard.
+
+### Notes
+* http://www.deanishe.net/alfred-workflow/tutorial_1.html
+* http://computers.tutsplus.com/tutorials/alfred-workflows-for-beginners--mac-55446
---
+++ uuid_flow.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+import sys
+import uuid
+from workflow import Workflow
+
+def main(wf):
+    val = "%s" % uuid.uuid4()
+    wf.add_item(title=val, valid=True, arg = val)
+    wf.send_feedback()
+
+if __name__ == u"__main__":
+    wf = Workflow(update_settings={'version': '0.1', 'github_slug': 'cholick/uuid_alfred_flow'})
+    sys.exit(wf.run(main))
